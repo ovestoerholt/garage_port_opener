9805dfd7baa82496ab0ab3653154f0dff2381e56
Ove Stoerholt <ovestoerholt@gmail.com>
2021-12-13 00:55:05 +0100

    Made it possible to blink LED the number of times specified by command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+use rust_gpiozero::*;
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    let hellos = std::env::args().nth(1).expect("You have to specify number of hello's...");
-    let args = Cli{
-        hellos: hellos.parse().expect("Not a number")
+    let blinks = std::env::args()
+        .nth(1)
+        .expect("You have to specify number of hello's...");
+    let args = Cli {
+        hellos: blinks.parse().expect("Not a number"),
     };
+
+    let led = LED::new(18);
+
     for n in 0..args.hellos {
-        print!("Hello {}\n", n);
+        print!("Blink! {}\n", n);
+        led.on();
+        sleep(Duration::from_secs(1));
+        led.off();
+        sleep(Duration::from_secs(1));
     }
 }
-
 
 struct Cli {
     hellos: i8,
